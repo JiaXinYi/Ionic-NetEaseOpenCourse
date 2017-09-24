add6180e00c4193287ea1b36c3f58926d69d0dc0
jiaxin <924463684@qq.com>
2017-09-24 19:52:45 +0800

    修改home和增加order

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 })
 export class MyApp {
   
-  rootPage:any = "HelloPage";
+  rootPage:any = "TabsPage";
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
